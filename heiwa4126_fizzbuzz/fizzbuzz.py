53edc241b41d0062cb4482177e9f1cc073798ce4
Masato Ota <heiwa4126@gmail.com>
2024-02-22 18:22:57 +0900

    Refactor. 55% faster (maybe)

--- heiwa4126_fizzbuzz/fizzbuzz.py
+++ heiwa4126_fizzbuzz/fizzbuzz.py
@@ -19,7 +19,7 @@ def fizzbuzz(n: int) -> Generator[str, None, None]:
         ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz']
     """
     for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
+        if i % 15 == 0:
             yield "FizzBuzz"
         elif i % 3 == 0:
             yield "Fizz"
